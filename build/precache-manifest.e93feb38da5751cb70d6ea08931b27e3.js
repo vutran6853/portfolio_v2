e1ac4e20f7c6dad18196fa88c9c354c0fe3b4226
Vu Tran <vutran6853@gmail.com>
2019-08-13 23:21:08 -0500

    build

---
+++ build/precache-manifest.e93feb38da5751cb70d6ea08931b27e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1a36d9388004f567654904d5676910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2673aa492309e8071f7f",
+    "url": "/static/css/main.fae64d75.chunk.css"
+  },
+  {
+    "revision": "d4b4257c24ce16731419",
+    "url": "/static/js/2.700723de.chunk.js"
+  },
+  {
+    "revision": "2673aa492309e8071f7f",
+    "url": "/static/js/main.638216d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23ddb528f193ae9ffbae7c2ed66b2055",
+    "url": "/static/media/clone-me.23ddb528.PNG"
+  },
+  {
+    "revision": "911bf3c844ff1abe4629fba2fc77d12a",
+    "url": "/static/media/clone-va.911bf3c8.PNG"
+  },
+  {
+    "revision": "6b117a8910aaa1bcc950bf8ee643e630",
+    "url": "/static/media/cryptoMarkets.6b117a89.PNG"
+  },
+  {
+    "revision": "66288d75abe848d6e0b917b561da49ad",
+    "url": "/static/media/kitnetyExchange1.66288d75.PNG"
+  },
+  {
+    "revision": "e8c7fb25c7c4c90f3f74237ad8484947",
+    "url": "/static/media/me.e8c7fb25.jpg"
+  },
+  {
+    "revision": "8bfd55e0f5a01dbdb3ae68f73f8fdec6",
+    "url": "/static/media/socialDashboard.8bfd55e0.PNG"
+  },
+  {
+    "revision": "da71c5167857b17c3cbb6acd649bb0fd",
+    "url": "/static/media/yelpclone1.da71c516.PNG"
+  }
+]);
